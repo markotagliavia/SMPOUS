0b7d10419215c2d935e5d84811cb23e1f922e420
Marko Tagliavia <max.tagliavia@gmail.com>
2019-01-10 23:32:12 +0100

    zuul hystrix readed timeout change

--- SMPOUS_UserService/src/main/java/smpous/models/Address.java
+++ SMPOUS_UserService/src/main/java/smpous/models/Address.java
@@ -16,17 +16,19 @@ public class Address implements Serializable{
 		
 	}
 	
-	public Address(String street, String number, GeoJsonPoint location) {
+	public Address(String street, String number, double x, double y) {
 		super();
+		GeoJsonPoint locationS = new GeoJsonPoint(x,y);
 		this.street = street;
 		this.number = number;
-		this.location = location;
+		this.location = locationS;
 	}
 	private String street;
 	private String number;
 	@GeoSpatialIndexed(type=GeoSpatialIndexType.GEO_2DSPHERE)
 	private GeoJsonPoint location;
-	
+	private double x;
+	private double y;
 	public GeoJsonPoint getLocation() {
 		return location;
 	}
@@ -46,6 +48,22 @@ public class Address implements Serializable{
 	}
 	public void setNumber(String number) {
 		this.number = number;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
 	}
 	
 }
