ae030403dae476b4d0014df91c56a9337578a29f
markod <dobric.marko23@gmail.com>
2019-01-15 22:01:50 +0100

    Ne radi, a mozda je do kompa

---
+++ SMPOUS_FilmService/src/main/java/sumpous/repository/MovieRepository.java
@@ -0,0 +1,16 @@
+package sumpous.repository;
+
+import java.util.HashSet;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import smpous.models.Movie;
+
+
+
+public interface MovieRepository extends MongoRepository<Movie, String>{
+
+	//public HashSet<Movie> findMovieByName(String name);
+	
+	
+}
